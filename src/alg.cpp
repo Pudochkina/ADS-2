04da416ac9999dd25fb258a64246653104849229
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-02 21:10:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,14 +21,14 @@ return result;
 }
 
 double calcItem(double x, uint16_t n) {
-double result = pown(x,n)/fact(n);
+double result = pown(x, n)/fact(n);
 return result;
 }
 
 double expn(double x, uint16_t count) {
 double result = 1;
-for (uint16_t i = 1; i <= count; i++){
-result += calcItem(x,i); 
+for (uint16_t i = 1; i <= count; i++) {
+result += calcItem(x, i);
 }
 return result;
 }
@@ -36,7 +36,7 @@ return result;
 double sinn(double x, uint16_t count) {
 double result = 0;
 for (uint16_t i = 1; i <= count; i++) {
-result += pown(-1,(i-1))*calcItem(x,(2*i-1));
+result += pown(-1, (i-1))*calcItem(x, (2*i-1));
 }
 return result;
 }
@@ -44,7 +44,7 @@ return result;
 double cosn(double x, uint16_t count) {
 double result = 0;
 for (uint16_t i = 1; i <= count; i++) {
-result += pown(-1,(i-1))*calcItem(x,(2*i-2));
+result += pown(-1, (i-1))*calcItem(x, (2*i-2));
 }
 return result;
 }
